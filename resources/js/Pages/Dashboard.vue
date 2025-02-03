c21bec98a4d5dd47b741b3df9a9d75a857d91c0b
Jhoan Avila <108818345+jhoanrcode@users.noreply.github.com>
2025-02-03 16:10:33 -0500

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -10,7 +10,7 @@ import { Head } from '@inertiajs/vue3';
 		<div class="container flex flex-col items-center gap-6 px-6 mx-auto text-center text-gray-700">
 			<h4 class="text-3xl font-semibold ">Bienvenid@ !</h4>
 			<p class="text-xl">{{ $page.props.auth.user.name }}</p>
-			<p class="pt-4">De acuerdo con la prueba tecnica propuesta, este pequeño administrador te permitira realizar un CRUD para la gestion de empleados y cargos. <br><b>Recuerda:</b> Añade de primeras los datos personales del empleado y despues asocialo a un cargo. <br><i>(Cada empleado podra ser asociado a un unico cargo).</i></p>
+			<p class="pt-4">Este pequeño administrador te permitira realizar un CRUD para la gestion de empleados y cargos. <br><b>Recuerda:</b> Añade de primeras los datos personales del empleado y despues asocialo a un cargo. <br><i>(Cada empleado podra ser asociado a un unico cargo).</i></p>
 			<p class="pt-4">Mas información del proyecto: <a :href="route('about')" class="text-indigo-600">About me</a></p>
 			<p class="pt-4"><small><i>Desarrollado por Jhoan Avila / joria94@hotmail.com / <a href="https://github.com/jhoanrcode" class="text-indigo-600" target="_blank">@jhoanrcode</a></i></small></p>
 		</div>
